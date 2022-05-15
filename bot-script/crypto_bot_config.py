d9d73cc3e18ea0dc2da20d99fb54f55f67702842
wannabebotter <WannabeCryptoBotter@gmail.com>
2022-05-15 17:02:26 +0000

    Added wm and tm parameters

--- bot-script/crypto_bot_config.py
+++ bot-script/crypto_bot_config.py
@@ -1,4 +1,5 @@
 from os import environ
+from datetime import timedelta
 
 # TimescaleDBのパラメータ
 pg_config = {
@@ -8,20 +9,43 @@ pg_config = {
     'port': environ['POSTGRES_PORT']
 }
 
+# ExchangeManagerのパラメータ
 binance_testnet_config = {
     'exchange_name': 'binanceusdm(testnet)',
     'rest_baseurl': 'https://testnet.binancefuture.com',
     'ws_baseurl': 'wss://stream.binancefuture.com',
 }
 
+# ExchangeManagerのパラメータ
 binance_config = {
     'exchange_name': 'binanceusdm',
     'rest_baseurl': 'https://fapi.binance.com',
     'ws_baseurl': 'wss://fstream.binance.com',
 }
 
-# PyBottersのパラメータ
+# PyBottersManagerのパラメータ
 pybotters_apis = {
     'binance': [environ['BINANCE_APIKEY'], environ['BINANCE_APISECRET']],
     'binance_testnet': [environ['BINANCE_TESTNET_APIKEY'], environ['BINANCE_TESTNET_APISECRET']]
+}
+
+#WeightManagerのパラメータ
+wm_config = {
+    'prohibit_symbol': ['LUNAUSDT'],
+    'timebar_db_name': 'TimebarManager',
+    'timebar_table_name': 'binanceusdm_timebar_5m',
+    'components_num': 16,
+    'risk_aversion': 2.0,
+    'l2_gamma': 0.02,
+    'rebalance_interval': timedelta(minutes = 15),
+    'rebalance_calc_range': timedelta(days = 2)
+}
+
+# TradeManagerのパラメータ
+tm_config = {
+    'weight_table_name': 'binanceusdm_weight_5m',
+    'weight_db_name': 'WeightManager',
+    'trade_interval': timedelta(minutes = 5),
+    'rebalance_time': timedelta(minutes = 10),
+    'components_num': wm_config['components_num']
 }
