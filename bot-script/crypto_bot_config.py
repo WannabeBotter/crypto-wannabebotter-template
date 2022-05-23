0289d2ac16514499f08bd3fa27f5d3974eec6d3a
wannabebotter <WannabeCryptoBotter@gmail.com>
2022-05-23 15:00:43 +0000

    Kafkaのチャネル名前付けルールに準拠

--- bot-script/crypto_bot_config.py
+++ bot-script/crypto_bot_config.py
@@ -11,14 +11,14 @@ pg_config = {
 
 # ExchangeManagerのパラメータ
 binance_testnet_config = {
-    'exchange_name': 'binanceusdm(testnet)',
+    'exchange_name': 'binanceusdm-testnet',
     'rest_baseurl': 'https://testnet.binancefuture.com',
     'ws_baseurl': 'wss://stream.binancefuture.com',
 }
 
 # ExchangeManagerのパラメータ
 binance_config = {
-    'exchange_name': 'binanceusdm',
+    'exchange_name': 'binanceusdm-mainnet',
     'rest_baseurl': 'https://fapi.binance.com',
     'ws_baseurl': 'wss://fstream.binance.com',
 }
@@ -37,7 +37,7 @@ wm_config = {
     'components_num': 16,
     'risk_aversion': 2.0,
     'l2_gamma': 0.02,
-    'rebalance_interval': timedelta(minutes = 30),
+    'rebalance_interval': timedelta(days = 1),
     'rebalance_calc_range': timedelta(days = 2)
 }
 
@@ -46,6 +46,6 @@ tm_config = {
     'weight_table_name': 'binanceusdm_weight_5m',
     'weight_db_name': 'WeightManager',
     'trade_interval': timedelta(minutes = 5),
-    'rebalance_time': timedelta(minutes = 20),
+    'rebalance_time': timedelta(hours = 2),
     'components_num': wm_config['components_num']    
 }
